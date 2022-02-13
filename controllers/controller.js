38c4fa5a3aef0790253a7e80896c21667b41e1b5
James Andrew Perez <75008298+NRu0512@users.noreply.github.com>
2022-02-13 18:48:50 +0800

    movies partial

--- controllers/controller.js
+++ controllers/controller.js
@@ -1,7 +1,27 @@
 const controller = {
   // Open index.hbs
   getIndex: function (req, res) {
-    res.render(`index`);
+    let movies = [];
+
+    movies.push({
+      row: 1,
+      title: "The Thing",
+      year: "1982",
+      rating: "8.1"
+    });
+
+    movies.push({
+      row: 2,
+      title: "Seven Samurai",
+      year: "1954",
+      rating: "8.6"
+    });
+
+    let content = {
+      movies: movies
+    };
+
+    res.render(`index`, content);
   },
 };
 
